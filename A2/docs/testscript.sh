4c572f3b859f63820c3d7be9c01a638f95b24bd9
Ben Soer <ben@soernet.ca>
2016-01-29 18:48:33 +0000

    added docs folder and test scripts

---
+++ A2/docs/testscript.sh
@@ -0,0 +1,20 @@
+
+
+echo "======================="
+echo "     Firewall Test     "
+echo "======================="
+echo "Where Is This Test Script Being Executed From? (Enter Number)"
+echo "1) From The Firewall Machine"
+echo "2) From The Internal Machine"
+
+read ANSWER returnKey
+if [ $ANSWER -eq 1]
+then
+    echo "Now Executing testfromexternal Tests"
+    ./testfromexternal.sh
+fi
+if [ $ANSWER -eq 2]
+then
+    echo "Now Executing testfrominternal Tests"
+    ./testfrominternal.sh
+fi
