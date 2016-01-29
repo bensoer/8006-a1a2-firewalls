00669e9491cf87a468071d2c979d21c6df220363
Ben Soer <ben@soernet.ca>
2016-01-29 17:47:24 +0000

    added more if statement quoting

--- A2/bfwall2.sh
+++ A2/bfwall2.sh
@@ -142,13 +142,13 @@ echo "Explcit Denial of External Traffic Matching Internal Traffic IP's"
 $IPTABLES -N tcp_traffic
 
 #OUTBOUND
-if [ $VALID_TCP_OUTBOUND_PORTS != "" ]
+if [ "$VALID_TCP_OUTBOUND_PORTS" != "" ]
 then
     $IPTABLES -A tcp_traffic -p tcp -i $IINTERNAL_NET -m multiport --source-ports $UNPRIV_PORTS -m multiport --destination-ports $VALID_TCP_OUTBOUND_PORTS -j is_new_and_established
     $IPTABLES -A tcp_traffic -p tcp -i $IEXTERNAL_NET -m multiport --source-ports $VALID_TCP_OUTBOUND_PORTS -m multiport --destination-ports $UNPRIV_PORTS -j is_established
 fi
 #INBOUND
-if [ $VALID_TCP_INBOUND_PORTS != "" ]
+if [ "$VALID_TCP_INBOUND_PORTS" != "" ]
 then
     $IPTABLES -A tcp_traffic -p tcp -i $IEXTERNAL_NET -m multiport --source-ports $UNPRIV_PORTS -m multiport --destination-ports $VALID_TCP_INBOUND_PORTS -j is_new_and_established
     $IPTABLES -A tcp_traffic -p tcp -i $IINTERNAL_NET -m multiport --source-ports $VALID_TCP_INBOUND_PORTS -m multiport --destination-ports $UNPRIV_PORTS -j is_established
